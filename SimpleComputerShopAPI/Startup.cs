3e6151bcf7abbc0af9941bc4037cae7eafc25f4a
mdrizy96 <duncanokeno@gmail.com>
2021-03-05 22:49:18 -0800

    Implement global error handling middleware

--- SimpleComputerShopAPI/Startup.cs
+++ SimpleComputerShopAPI/Startup.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using Contracts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
@@ -32,12 +33,14 @@ namespace SimpleComputerShopAPI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerManager logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.ConfigureExceptionHandler(logger);
 
             app.UseStaticFiles();
             app.UseCors("CorsPolicy");
